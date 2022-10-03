8a35eeabbd14fd72dd144379597971868885001c
Mateus Nunes Campos <mncampos@inf.ufrgs.br>
2022-10-03 20:51:09 -0300

    Add files via upload

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -6,60 +6,123 @@ const express = require("express");
 const { db } = require("../models/userSchema");
 const router = express.Router();
 
-const procedureManager = "Richard Leal";
-const procedureWorker1 = "Jorge"; // Resto
-const procedureWorker2 = "Orlando"; // Responsável por consulta médica, exame de sangue, tratamento dentário
-const procedureWorker3 = "Xuxa"; // Responsável por banho ou tosa
-
+const procedureManager = "Richard Admin";
 
 router.post("/addPet", async (req, res) => {
-  const {name, age, weight, specie, owner} = req.body;
+  const { name, age, weight, specie, owner } = req.body;
   if (!name || !age || !weight || !specie)
-    return res.status(400).json({msg:"Please complete all fields before submitting."});
-  const newPet = new petSchema({name, age, specie, weight, owner});
+    return res
+      .status(400)
+      .json({ msg: "Please complete all fields before submitting." });
+  const newPet = new petSchema({ name, age, specie, weight, owner });
   const newPetRes = await newPet.save();
 
-  if(newPetRes)
-    return res.status(200).json({msg : "Pet is succesfully saved on the database."});
-})
+  if (newPetRes)
+    return res
+      .status(200)
+      .json({ msg: "Pet is succesfully saved on the database." });
+});
 
 router.post("/registerProcedure", async (req, res) => {
-  const {name, pet, petOwner, petOwnerEmail} = req.body;
+  const { name, pet, petOwner, petOwnerEmail } = req.body;
   const procedureStartDate = Date.now();
-  let procedureWorker = '';
 
-  if(name === 'Banho' || name === 'Tosa')
-    procedureWorker = procedureWorker3;
-  else if(name === 'Tratamento Dentário' || name === 'Exame de Sangue' || name === 'Exame Médico')
-    procedureWorker = procedureWorker2;
-    else procedureWorker = procedureWorker1;
+  const procedureState = "Pending Approval";
+  const procedureWorker = "To be delegated";
 
-  const procedureState = 'Pending Approval';
-  
-  const newProcedure = new procedureSchema({name, pet, petOwner, petOwnerEmail, procedureManager, procedureWorker, procedureStartDate, procedureState});
+  const newProcedure = new procedureSchema({
+    name,
+    pet,
+    petOwner,
+    petOwnerEmail,
+    procedureManager,
+    procedureWorker,
+    procedureStartDate,
+    procedureState,
+  });
   const newProcedureRes = await newProcedure.save();
 
-  if(newProcedureRes)
-    return res.status(200).json({msg : "Procedure is succesfully saved on the database."});
-
-})
+  if (newProcedureRes)
+    return res
+      .status(200)
+      .json({ msg: "Procedure is succesfully saved on the database." });
+});
 
 router.post("/getProcedures", async (req, res) => {
-  const { owner, pet} = req.body;
-  procedureSchema.find({pet: pet, petOwner: owner}).lean().exec(function(err, docs) {
+  const { owner, pet } = req.body;
+  procedureSchema
+    .find({ pet: pet, petOwner: owner })
+    .lean()
+    .exec(function (err, docs) {
+      if (!err) return res.status(200).json(docs);
+    });
+});
+
+router.post("/getEmployeeDelegatedProcedures", async (req, res) => {
+  const {employee} = req.body;
+  procedureSchema.find({procedureState: "Delegated", procedureWorker: employee}).lean().exec(function(err, docs) {
     if(!err)
     return res.status(200).json(docs);
-    
+  });
+})
+
+router.get("/getDelegatedProcedures", async (req, res) => {
+  procedureSchema
+    .find({ procedureState: "Delegated" })
+    .lean()
+    .exec(function (err, docs) {
+      if (!err) return res.status(200).json(docs);
+    });
+});
+
+router.get("/getEmployees", async (req, res) => {
+  userSchema
+    .find({ userType: "Employee" })
+    .lean()
+    .exec(function (err, docs) {
+      if (!err) return res.status(200).json(docs);
+    });
+});
+
+router.get("/getOpenProcedures", async (req, res) => {
+  procedureSchema
+    .find({ procedureState: "Pending Approval" })
+    .lean()
+    .exec(function (err, docs) {
+      if (!err) return res.status(200).json(docs);
+    });
+});
+
+router.post("/updateProcedure", async (req, res) => {
+  const { _id, employee } = req.body;
+
+  procedureSchema
+    .findOneAndUpdate(
+      { _id: _id },
+      { procedureWorker: employee, procedureState: "Delegated" }
+    )
+    .exec(function (err, docs) {
+      if (!err) return res.status(200).json({ msg: "Updated succesfully!" });
+    });
+});
+
+router.post("/completeProcedure", async (req, res) => {
+  const {_id } = req.body;
+
+  procedureSchema.findOneAndUpdate({_id: _id}, {procedureState: "Complete"}).exec(function(err,docs) {
+    if (!err) return res.status(200).json({ msg: "Updated succesfully!"});
   })
 })
 
 router.post("/getPets", async (req, res) => {
-    const {owner} = req.body;
-    petSchema.find({owner}).lean().exec(function(err,docs) {
-      if(!err)
-      return res.status(200).json(docs);
+  const { owner } = req.body;
+  petSchema
+    .find({ owner })
+    .lean()
+    .exec(function (err, docs) {
+      if (!err) return res.status(200).json(docs);
     });
-})
+});
 
 router.post("/register", async (req, res) => {
   const { email, password, userType, address, name } = req.body;
@@ -81,7 +144,7 @@ router.post("/register", async (req, res) => {
 
     if (savedUserRes)
       return res
-        .status(200)    
+        .status(200)
         .json({ msg: "User is succesfully saved on the database." });
   });
 });
@@ -97,9 +160,12 @@ router.post("/login", async (req, res) => {
   }
   const matchPassword = await bcrypt.compare(password, user.password);
   if (matchPassword) {
-
-    const userSession = {email: user.email, name: user.name}
-    req.session.user = userSession
+    const userSession = {
+      email: user.email,
+      name: user.name,
+      userType: user.userType,
+    };
+    req.session.user = userSession;
 
     return res.status(200).json({ msg: "Logged in succesfully!", userSession });
   } else {
@@ -107,13 +173,13 @@ router.post("/login", async (req, res) => {
   }
 });
 
-router.delete('/logout', (req, res) => {
+router.delete("/logout", (req, res) => {
   if (req.session) {
-    req.session.destroy(err => {
-      if(err){
-        res.status(400).json({msg:"Unable to log out."})
+    req.session.destroy((err) => {
+      if (err) {
+        res.status(400).json({ msg: "Unable to log out." });
       } else {
-        res.status(200).json({msg:"Logout successful."})
+        res.status(200).json({ msg: "Logout successful." });
       }
     });
   } else {
@@ -122,24 +188,22 @@ router.delete('/logout', (req, res) => {
 });
 
 router.delete("/cancelProcedure", (req, res) => {
-  const {id} = req.body;
-  procedureSchema.findByIdAndDelete(id, function(err, docs) {
-    if (err){
-      res.status(400).json({msg:"Unable to delete procedure - not found."});
+  const { id } = req.body;
+  procedureSchema.findByIdAndDelete(id, function (err, docs) {
+    if (err) {
+      res.status(400).json({ msg: "Unable to delete procedure - not found." });
+    } else {
+      res.status(200).json({ msg: "Procedure canceled with success !" });
     }
-    else{
-      res.status(200).json({msg:"Procedure canceled with success !"});
-    }
-  })
-
+  });
 });
 
-router.get('/isAuth', async (req, res) => {
+router.get("/isAuth", async (req, res) => {
   if (req.session.user) {
-    return res.json(req.session.user)
+    return res.json(req.session.user);
   } else {
-    return res.status(401).json('unauthorize')
+    return res.status(401).json("unauthorize");
   }
-})
+});
 
 module.exports = router;
