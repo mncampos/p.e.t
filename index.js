0c48773ad218d20a50932f7b4b535f7bf4a2fa4d
Mateus Nunes Campos <mncampos@inf.ufrgs.br>
2022-10-03 17:12:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-require('dotenv').config({path: "./config.env"})
+require('dotenv').config();
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const session = require('express-session')
 const MongoDBStore = require('connect-mongodb-session')(session)
-
+var path = require("path");
+const port = process.env.PORT || 5000
 const MAX_TIME = 1000 * 60 * 60 * 3 //Tempo que o usuário ficará logado -> 3h
 
+const uri = process.env.DATABASE_CONNECTION_STRING;
+
 const mongoDBStore = new MongoDBStore({
-    uri: process.env.DATABASE_CONNECTION_STRING,
+    uri: uri,
     collection: 'sessions',
 })
 
@@ -17,12 +20,12 @@ const mongoDBStore = new MongoDBStore({
 const loginRouter = require('./routes/loginRoutes')
 
 const app = express()
-const port = process.env.PORT || 5000
+
 
 
 
 mongoose.Promise = global.Promise
-mongoose.connect(process.env.DATABASE_CONNECTION_STRING, {
+mongoose.connect(uri, {
   useNewUrlParser: true,
 }, (err) => { if (!err) console.log('Database connected succesfully!')})
 
@@ -41,9 +44,16 @@ app.use(session({
 )
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'build')));
+
 
 // ROUTERS
 app.use('/api', loginRouter)
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 
 // START SERVER
 app.listen(port, () => {
